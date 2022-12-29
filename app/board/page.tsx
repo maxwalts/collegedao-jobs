d13972134e9b901527030fb4c9d93ab77dd497c1
Max Walts <mwalts@umich.edu>
2022-12-29 17:19:19 -0600

    hosting

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import Image from "next/image";
 import { useState } from "react";
+import Categories from "@/components/board/categories";
 import DetailView from "../../components/board/detail-view";
 import Job from "../../components/board/job";
 
@@ -59,32 +59,7 @@ function JobBoard({ jobs, }: { jobs: any[] }) {
   )
 }
 
-// button needs value prop can pass information to state
-// TODO: continue to be white after click
-function Categories({ selectedCategory, setSelectedCategory }: any) {
 
-  return (
-    <div className="flex flex-row space-x-5">
-      <CategoryItem value="all" onSelectedCategoryChange={setSelectedCategory} isSelected={selectedCategory == "all"} />
-      <CategoryItem value="marketing" onSelectedCategoryChange={setSelectedCategory} isSelected={selectedCategory == "marketing"} />
-      <CategoryItem value="technical" onSelectedCategoryChange={setSelectedCategory} isSelected={selectedCategory == "technical"} />
-      <CategoryItem value="design" onSelectedCategoryChange={setSelectedCategory} isSelected={selectedCategory == "design"} />
-    </div >
-  );
-};
-
-function CategoryItem({ value, onSelectedCategoryChange, isSelected }: any) {
-  const handleClick = (e: any) => {
-    onSelectedCategoryChange(e.target.value);
-  }
-
-  var className = isSelected ? "text-stone-200" : "text-stone-400";
-  className += " hover:text-stone-200";
-
-  return (
-    <button onClick={handleClick} value={value} className={className}>{value.charAt(0).toUpperCase() + value.slice(1)}</button>
-  )
-}
 
 interface Job {
   id: number;
